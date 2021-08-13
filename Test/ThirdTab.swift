5226701ada61b32753485b55f8da46c955432ea6
ShayantanSantra <shayantan.santra@gmail.com>
2021-08-13 17:59:15 +0530

    Pinch

--- Test/ThirdTab.swift
+++ Test/ThirdTab.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class ThirdTab: UIViewController {
     @IBOutlet var switch1: UISwitch!
+    
+    let Imageview = UIImageView()
+    let pinchgesture = UIPinchGestureRecognizer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Imageview.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
+        Imageview.image = #imageLiteral(resourceName: "Random_image")
+        Imageview.isUserInteractionEnabled = true
+        self.view.addSubview(Imageview)
+        Imageview.center = view.center
+        
+        Imageview.addGestureRecognizer(pinchgesture)
+        pinchgesture.addTarget(self, action: #selector(pinchaction))
         //navigationItem.leftBarButtonItems = UIBarButtonItem(title: "back", style: .plain, target: self, action: #selector(goback))
         //navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(goback))
         // Do any additional setup after loading the view.
     }
+   /* private func addgesture(){
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(didpinch()))
+        view.addGestureRecognizer(pinch)
+    }*/
+    @objc private func pinchaction(){
+        guard let gestureview = pinchgesture.view else{
+            return
+        }
+        gestureview.transform = gestureview.transform.scaledBy(x: pinchgesture.scale, y: pinchgesture.scale)
+        pinchgesture.scale = 1
+        }
+    
     
     @IBAction func switchchange(_ sender: UISwitch) {
         if sender.isOn{
@@ -44,3 +69,4 @@ class ThirdTab: UIViewController {
     */
 
 }
+
