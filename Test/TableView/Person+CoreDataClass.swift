71a6938a1e3b0477d124e79b9a8dfa6fbc83d888
ShayantanSantra <shayantan.santra@gmail.com>
2021-08-16 22:21:58 +0530

    Insert, update, delete CRUD operations

---
+++ Test/TableView/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  Test
+//
+//  Created by Shayantan Santra on 16/08/21.
+//  Copyright © 2021 Shayantan Santra. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
